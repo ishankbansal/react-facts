9407ca19fe6db484508e53408735426cebda0fc9
Ishank Bansal <ishankbansal1111@gmail.com>
2022-04-14 23:38:50 +0530

    one more step ahead

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,12 @@
 
 export default function Navbar(){
-    return <h1>Navbar component</h1>
+    return (
+        <nav>
+            <img alt="" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-1024.png" className="nav--icon"/>
+            <h3 className="nav--logo_text">React Facts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
 }
+
+//https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-1024.png
