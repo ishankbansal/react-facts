ca5d905420d3b294af4f3bd2ab31db5b43386171
Ishank Bansal <ishankbansal1111@gmail.com>
2022-06-09 07:59:53 +0530

    completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-
+import ReactImg from "../images/React-icon.svg.png"
 export default function Navbar(){
     return (
         <nav>
-            <img alt="" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-1024.png" className="nav--icon"/>
+            <img alt="" src={ReactImg} className="nav--icon"/>
             <h3 className="nav--logo_text">React Facts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
